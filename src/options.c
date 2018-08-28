fe5430f477390d8b45868f00b20ed47c670b5b50
k21971 <keith.simpson1971@gmail.com>
2018-08-28 18:52:56 -0400

    Patch: Dark Room v1.0 by Clive Crous

--- src/options.c
+++ src/options.c
@@ -723,6 +723,11 @@ initoptions()
 	/* result in the player's preferred fruit [better than "\033"].	*/
 	obj_descr[SLIME_MOLD].oc_name = "fruit";
 
+        if (flags.lit_corridor && iflags.use_color) {
+            showsyms[S_darkroom]=showsyms[S_room];
+        } else {
+            showsyms[S_darkroom]=showsyms[S_stone];
+        }
 	return;
 }
 
@@ -3018,6 +3023,7 @@ goodfruit:
 			     */
 			    vision_recalc(2);		/* shut down vision */
 			    vision_full_recalc = 1;	/* delayed recalc */
+			    if (iflags.use_color) need_redraw = TRUE;  /* darkroom refresh */
 			}
 			else if ((boolopt[i].addr) == &iflags.use_inverse ||
 					(boolopt[i].addr) == &iflags.showrace ||
@@ -3333,8 +3339,14 @@ doset()
 	}
 
 	destroy_nhwindow(tmpwin);
-	if (need_redraw)
+	if (need_redraw) {
+	    if (flags.lit_corridor && iflags.use_color) {
+		showsyms[S_darkroom]=showsyms[S_room];
+	    } else {
+		showsyms[S_darkroom]=showsyms[S_stone];
+	    }
 	    (void) doredraw();
+	}
 	return 0;
 }
 
